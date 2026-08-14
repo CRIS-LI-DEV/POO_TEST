159d75b56a29daa31fb71ed8761dd0dfe5efcc67
CRISTIAN <131208714+CRIS-LI-DEV@users.noreply.github.com>
2026-08-13 21:45:04 -0400

    nuevo archivo

---
+++ src/Mascota.java
@@ -0,0 +1,17 @@
+
+public class Mascota {
+	String nombre ="";
+	String raza ="";
+	int edad =0;
+	
+	//metodos 
+	void comer() {
+		System.out.print("ESTA COMIENDO");
+	}
+	
+	void jugar() {
+		System.out.print("ESTA JUGANDO");
+	}
+	
+	
+}
